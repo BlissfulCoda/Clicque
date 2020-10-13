d3271a91f8fba94f3cc4dd9ad950663ad3e77e8d
Ronnie kiyegga <Ronniekiyegga@hotmail.com>
2020-10-13 23:02:04 +0100

    using an IIFE for lexical scope

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const {app, BrowserWindow} = require('electron')
+(function() {
+  const { app, BrowserWindow } = require('electron');
 
-//Main window
-function createMainWindow(){
-    const mainWindow = new BrowserWindow({
-        title: 'ImageShrink',
-        width: 400,
-        height: 500
-    })
-}
 
-app.on('ready', createMainWindow);
+  let mainWindow;
+  //Creating the Main window
+  function createMainWindow() {
+    mainWindow = new BrowserWindow({
+      title: 'ImageShrink',
+      width: 400,
+      height: 500
+    });
+
+    mainWindow.loadURL('https://weareadaptive.com/')
+  }
+
+  app.on('ready', createMainWindow);
+})();
