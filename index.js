4dd527f3cbb70170e779cd3f84e3cf17d5ac2073
Ronnie kiyegga <Ronniekiyegga@hotmail.com>
2020-10-14 00:11:36 +0100

    adding the necessary structure of the app

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 (function() {
   const { app, BrowserWindow } = require('electron');
 
+  //set Environment
+  process.env.NODE_ENV = 'development'
+
+
+  const isDev = process.env.NODE_ENV !== 'production' ? true : false
+  const isMac = process.platform === 'darwin' ? true : false
 
   let mainWindow;
   //Creating the Main window
   function createMainWindow() {
     mainWindow = new BrowserWindow({
-      title: 'ImageShrink',
+      title: 'Clique',
       width: 400,
-      height: 500
+      height: 500,
+      resizable: isDev
     });
 
-    mainWindow.loadURL('https://weareadaptive.com/')
+    mainWindow.loadFile('./app/index.html')
   }
 
   app.on('ready', createMainWindow);
+  app.on('window-all-closed', () => {
+      if(!isMac){
+          app.quit();
+      }
+  })
+
+  app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createMainWindow();
+    }
+})
+
+
+
+
+
 })();
